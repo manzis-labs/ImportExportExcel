ea37c29d4319e92bf74a4539350345901e8c30fa
Manzis <manzisilhammaulana@gmail.com>
2026-04-10 12:33:55 +0700

    npm install

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,20 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImportController;
 
-Route::get('/', function () {
-    return view('upload');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/', function () {
+        return view('upload');
+    });
+
+    Route::post('/preview', [ImportController::class, 'preview']);
+    Route::post('/import', [ImportController::class, 'import']);
+    Route::get('/dashboard', [ImportController::class, 'index']);
+    Route::get('/delete/{id}', [ImportController::class, 'delete']);
+
 });
 
-Route::post('/import', [ImportController::class, 'upload']);
-Route::post('/preview', [ImportController::class, 'preview']);
-Route::post('/import', [ImportController::class, 'import']);
-Route::get('/dashboard', [ImportController::class, 'index']);
-Route::get('/delete/{id}', [ImportController::class, 'delete']);
+Route::get('/', function () {
+    return redirect('/login');
+});
